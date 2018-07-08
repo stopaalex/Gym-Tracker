45f8b301b3347227e5bf83c586d4797f347890ae
Alex Stopa <stopa.alex@gmail.com>
2018-07-08 11:04:35 -0400

    UPDATE ::: First Push
    first push of files for gym tracker app

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/////////////////////////
+// Main script.js file //
+/////////////////////////
+
+// DECLARE VARIABLES
+// var
+
+// DECLARE FUNCTIONS
+// initialize();
+
+function initialize() {
+    console.log('Initialzie');
+}
+
+initialize();
